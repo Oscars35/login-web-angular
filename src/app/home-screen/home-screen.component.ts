ec4d087d35a2fd9b7c1f46e952feb036d880da1d
Oscar <oscar13785@gmail.com>
2022-12-29 11:42:08 +0100

    Schema done

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
   styleUrls: ['./home-screen.component.scss']
 })
-export class HomeScreenComponent {
+export class HomeScreenComponent implements OnInit {
 
+  constructor() {
+  }
+  ngOnInit() {
+    this.addButtonsHref()
+  }
+
+  private addButtonsHref() {
+    //I don't want to keep the state of the previous page, for that reason i use href instead of routerLink, that saves the state
+    //Of all the things of the previous pages.
+    let goLoginButton = document.getElementsByClassName('go-login-button')[0];
+    let goRegisterButton = document.getElementsByClassName('go-register-button')[0];
+    goLoginButton.addEventListener('click', () => {
+      window.location.href = '/login';
+    });
+    goRegisterButton.addEventListener('click', () => {
+      window.location.href = '/register';
+    });
+  }
 }
